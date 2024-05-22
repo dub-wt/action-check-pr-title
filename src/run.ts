43baa567d7c104acdae4fc6962849e51a1e3d734
Serge Dubrovin <serge.dubrovin@wetravel.com>
2024-05-22 16:55:38 +0200

    Allow to define multiple regexps

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,9 @@
 import { info, setFailed, getInput } from "@actions/core";
 import { Context } from "@actions/github/lib/context";
 
-
+function parseRegexList(input: string): string[] {
+  return input.split(/\r|\n/).map((line) => line?.trim());
+}
 
 export const run = (context: Context) => {
   const { eventName } = context;
@@ -16,15 +18,13 @@ export const run = (context: Context) => {
 
   info(`Pull Request title: "${pullRequestTitle}"`);
 
-  const regexParam: string | Array<string> = getInput("regexp");
+  const regexParam = getInput("regexp", { required: true });
   const flagsParam = getInput("flags");
   const helpMessage = getInput("helpMessage");
 
-  const regexpList = [];
-  regexpList.push(...regexParam);
+  const regexpList = parseRegexList(regexParam);
 
   let matches = false;
-
   for (const item of regexpList) {
     const regex = RegExp(item, flagsParam);
 
@@ -35,10 +35,18 @@ export const run = (context: Context) => {
   }
 
   if (!matches) {
-    let message = `Pull Request title "${pullRequestTitle}" failed to pass match any of ${regex} regexps.
-`;
+    let message = `Pull Request title "${pullRequestTitle}"`;
+
+    if (regexpList.length === 1) {
+      message += `fails to match the regex pattern: ${regexpList[0]}`;
+    } else {
+      message += `fails to match any of the regex patterns: [${regexpList.join(
+        ", "
+      )}]`;
+    }
+
     if (helpMessage) {
-      message = message.concat(helpMessage);
+      message += `\n${helpMessage}`;
     }
 
     setFailed(message);
